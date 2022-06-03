db3c9ee7a3ff1aa4f13d3ffd8401775b1e64058a
John Eastman <radon@Johns-MacBook-Pro.local>
2022-06-02 20:39:02 -0600

    variables/environments initial

--- evaluator.py
+++ evaluator.py
@@ -3,6 +3,13 @@ import tokenizer
 
 # TODO: Return a Value object for values (numbers, booleans, etc.) instead of the raw value so we can check the type
 #  and throw errors for incompatible types in comparison operators.
+GLOBAL = "GLOBAL"
+
+
+class Environment:
+    def __init__(self, parent_env):
+        self.variables = {}
+        self.parent_env = parent_env
 
 
 class Evaluator:
@@ -10,6 +17,14 @@ class Evaluator:
         self.ast = ast
         self.variables = {}
         self.functions = {}
+        self.current_scope = GLOBAL
+
+        self.env = {
+            GLOBAL: {}
+        }
+
+        # Global scope: functions and variables defined outside functions (in the main file)
+        # Each function has its own scope with its own variables
 
     def evaluate(self):
         return [self.evaluate_expression(expression) for expression in self.ast]
@@ -50,45 +65,50 @@ class Evaluator:
                 raise Exception(f"Invalid unary operator: {expression.op.type} ({expression.op})")
 
         elif type(expression) == _parser.AssignVariable:
-            self.variables[expression.name.value] = self.evaluate_expression(expression.value)
+            self.env[self.current_scope][expression.name.value] = self.evaluate_expression(expression.value)
             return "null"
 
         elif type(expression) == _parser.AssignFunction:
-            function_parameters = expression.parameters
-            self.functions[expression.name.value] = {"params": function_parameters, "statements": expression.statements}
+            self.functions[expression.name.value] = expression
             return "null"
 
         elif type(expression) == _parser.Identifier:
-            variable_value = self.variables.get(expression.value, None)
+            variables = self.env[self.current_scope]
+            variable_value = variables.get(expression.value, None)
             if variable_value is None:
                 raise Exception(f"Undefined variable at line {expression.line_num}: {expression.value}")
             return variable_value
 
         elif type(expression) == _parser.FunctionCall:
-            function = self.functions.get(expression.name.value, None)
+            function_name = expression.name.value
+            function = self.functions.get(function_name, None)
 
             # If the function is not defined in the functions dictionary, throw an error saying the
             # function is undefined
             if function is None:
-                raise Exception(f"Undefined function at line {expression.name.line_num}: {expression.name.value}")
+                raise Exception(f"Undefined function at line {expression.name.line_num}: {function_name}")
 
-            parameter_identifiers = function["params"]
+            parameter_identifiers = function.parameters
             parameter_values = expression.parameter_values
 
             if len(parameter_identifiers) != len(parameter_values):
                 error_msg = "Incorrect number of arguments. "
-                error_msg += f"Function '{expression.name.value}' defined with {len(parameter_identifiers)} parameters "
+                error_msg += f"Function '{function_name}' defined with {len(parameter_identifiers)} parameters "
                 error_msg += f"({', '.join(map(lambda t: t.value, parameter_identifiers))}), "
                 error_msg += f"but {len(parameter_values)} given."
                 raise Exception(error_msg)
 
+            self.current_scope = function_name
+            self.env[self.current_scope] = {}
             # Map the parameters to their values and set them in the variables dictionary
             for param_name, param_value in zip(parameter_identifiers, parameter_values):
                 self.evaluate_expression(_parser.AssignVariable(param_name, param_value))
 
             # Evaluate every expression in the function body
-            statements = function["statements"]
+            statements = function.statements
             executed_expressions = [self.evaluate_expression(expression) for expression in statements]
+
+            self.current_scope = GLOBAL
 
             return executed_expressions[-1] if type(statements[-1]) == _parser.Return else "null"
 
