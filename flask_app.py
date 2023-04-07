bdc1535203de353915cb10130d50c8588c16e423
John Eastman <jp.robotics@gmail.com>
2023-04-07 17:10:32 -0600

    reformat and clear code button

--- flask_app.py
+++ flask_app.py
@@ -18,8 +18,8 @@ def hello_world():
     return render_template("index.html", source_code=source_code, results=results)
 
 
-@app.route("/interpreter", methods=["POST"])
-def interpreter():
+@app.route("/interpret", methods=["POST"])
+def interpret():
     source_code = request.form["source"]
 
     results = evaluate(source_code, Environment())
@@ -27,6 +27,14 @@ def interpreter():
     resp = make_response(redirect("/"))
     resp.set_cookie("source_code", source_code)
     resp.set_cookie("results", json.dumps(list(map(str, results))))
+    return resp
+
+
+@app.route("/clear", methods=["POST"])
+def clear():
+    resp = make_response(redirect("/"))
+    resp.delete_cookie("source_code")
+    resp.delete_cookie("results")
     return resp
 
 
