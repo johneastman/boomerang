b34210ed29ce1b64ac86f05694c84b6d5ae03805
John Eastman <jp.robotics@gmail.com>
2023-04-16 14:44:33 -0600

    add syntax highlighting and formatting

--- static/editor.js
+++ static/editor.js
@@ -1,4 +1,24 @@
+CodeMirror.defineMode("boomerang", function() {
+    return {
+        token: function(stream,state) {
+            if (stream.match(/func|when|is|else|true|false/)) {
+                return "keywords";
+            } else if (stream.match(/\".*?\"/)) {
+                return "strings";
+            } else if (stream.match(/print/)) {
+                return "builtins";
+            } else if (stream.match(/[0-9]+(.?[0-9]+)*/)) {
+                return "numbers";
+            }
+
+            stream.next();
+            return null;
+        }
+    };
+});
+
 let myTextarea = document.getElementById("code-editor");
 let editor = CodeMirror.fromTextArea(myTextarea, {
+    mode: "boomerang",
     lineNumbers: true
 });
